4b75b6268150107d8ecbec936e4e2428a07cb27b
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2024-08-08 22:10:27 +0300

    Feat: Create chart view

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -15,34 +15,8 @@ import {
   Checkbox,
   FormControlLabel,
 } from "@mui/material"
-
-type TableDataType = {
-  created_dt: string
-  data_source_modified_dt: string
-  entity_type: string
-  operating_status: string
-  legal_name: string
-  dba_name: string
-  physical_address: string
-  p_street: string
-  p_city: string
-  p_state: string
-  p_zip_code: number
-  phone: string
-  mailing_address: string
-  m_street: string
-  m_city: string
-  m_state: string
-  m_zip_code: number
-  usdot_number: number
-  mc_mx_ff_number: string
-  power_units: number
-  mcs_150_form_date: string
-  drivers: number
-  mcs_150_mileage_year: string
-  id: number
-  credit_score: string | null
-}
+import { TableDataType } from "../types"
+import Chart from "./Chart"
 
 type DataTableProps = {
   data: TableDataType[]
@@ -269,6 +243,7 @@ const DataTable: React.FC<DataTableProps> = ({ data }) => {
           onRowsPerPageChange={handleChangeRowsPerPage}
         />
       </Box>
+      <Chart data={searchedData} />
     </Paper>
   )
 }
