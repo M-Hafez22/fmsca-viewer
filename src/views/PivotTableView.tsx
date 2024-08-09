454e28a9c9ce79f70d433be80684a7247d48fd5d
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2024-08-09 10:36:20 +0300

    Feat: sync Pivot chart with the pivot table

--- src/views/PivotTableView.tsx
+++ src/views/PivotTableView.tsx
@@ -20,43 +20,58 @@ const PivotTableView: React.FC<Props> = ({ data }) => {
   const [pivotState, setPivotState] = useState<any>({})
 
   const chartData = useMemo(() => {
-    const filteredData = applyFilters(pivotState, data) // Apply filters based on pivot state
-    return aggregateData(filteredData, "out_of_service_date", "month") // Aggregate by month
+    const filteredData = applyFilters(pivotState, data)
+    return aggregateData(
+      filteredData,
+      pivotState.rows,
+      "out_of_service_date",
+      "month"
+    )
   }, [pivotState, data])
 
   // Function to apply filters from the pivot table state
   function applyFilters(pivotState: any, data: TableDataType[]) {
-    // Implement your filtering logic here
-    // E.g., filter by entity_type or other criteria selected in the pivot table
-    return data
+    return data.filter(row => {
+      return Object.keys(pivotState.filters || {}).every(filterKey => {
+        const filterValues = pivotState.filters[filterKey]
+        return filterValues.includes(row[filterKey as keyof TableDataType])
+      })
+    })
   }
 
   // Function to aggregate data by the chosen time period
   function aggregateData<K extends keyof TableDataType>(
     data: TableDataType[],
-    dateField: K, // Ensure dateField is a key of TableDataType
+    groupByFields: string[] = [],
+    dateField: K,
     period: "month" | "year" | "week"
   ) {
-    const aggregated: { period: string; count: number }[] = []
-    // Aggregate logic based on period
+    const aggregated: Record<string, { period: string; count: number }> = {}
+
     data.forEach(item => {
       const dateValue = item[dateField]
-      if (!dateValue) {
-        return
-      }
-      const date = new Date(dateValue as string) // Cast to string assuming it's a date string
+      if (!dateValue) return
+
+      const date = new Date(dateValue as string)
       const key =
         period === "month"
           ? `${date.getFullYear()}-${date.getMonth() + 1}`
           : date.getFullYear().toString()
-      const found = aggregated.find(entry => entry.period === key)
-      if (found) {
-        found.count += 1
+
+      const groupByKey = groupByFields
+        .map(field => item[field as keyof TableDataType])
+        .join(" - ")
+
+      const finalKey = `${key}-${groupByKey}`
+
+      if (aggregated[finalKey]) {
+        aggregated[finalKey].count += 1
       } else {
-        aggregated.push({ period: key, count: 1 })
+        aggregated[finalKey] = { period: key, count: 1 }
       }
     })
-    return aggregated
+
+    return Object.values(aggregated)
   }
 
   return (
